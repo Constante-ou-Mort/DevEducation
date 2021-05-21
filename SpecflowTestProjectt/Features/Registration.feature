e146080809ce056ace851778957067fc0d7004a2
LopatkaDenys <qa.denys.lopatka@gmail.com>
2021-05-21 16:29:45 +0300

    Запушил

--- SpecflowTestProjectt/Features/Registration.feature
+++ SpecflowTestProjectt/Features/Registration.feature
@@ -5,10 +5,10 @@ In order to register in NewBookModels
 As a client of NewBookModels
 I want to sign up to NewBookModels
 
-    @mytag
-    Scenario: It is possible to sign up to NewBookModels with valid data
-        Given Sign up page is opened
-        When I sign up with data
-          | first_name   | last_name   | email         | password   | confirm_password | mobile     |
-          | Den | DenDen | 'UniqueEmail' | QWEqwe123@ | QWEqwe123@       | 5555555555 |
-        Then Next page is opened
+@mytag
+Scenario: It is possible to sign up to NewBookModels with valid data
+	Given Sign up page is opened
+	When I sign up with data
+	| first_name   | last_name   | email   | password   | confirm_password   | mobile   |
+	| <first_name> | <last_name> | <email> | <password> | <confirm_password> | <mobile> |
+	Then Next page is opened
--- SpecflowTestProjectt/Steps/UI/RegistrationSteps.cs
+++ SpecflowTestProjectt/Steps/UI/RegistrationSteps.cs
@@ -4,6 +4,7 @@ using SeleniumTests.POM;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 using System.Linq;
+using System;
 
 namespace SpecflowTestProject.Steps.UI
 {
@@ -11,20 +12,58 @@ namespace SpecflowTestProject.Steps.UI
     public class RegistrationSteps
     {
         private readonly ScenarioContext _scenarioContext;
-        private readonly RegistrationPage.RegistrationPage RegistrationPage;
+        private readonly RegistrationPage.RegistrationPage _registrationPage;
 
         public RegistrationSteps(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
             var webDriver = _scenarioContext.Get<IWebDriver>(Context.WebDriver);
-            RegistrationPage = new RegistrationPage.RegistrationPage(webDriver);
+            _registrationPage = new RegistrationPage.RegistrationPage(webDriver);
         }
         
         [Given(@"Sign up page is opened")]
         public void GivenSignUpPageIsOpened()
         {
-            RegistrationPage.OpenPage();
+            _registrationPage.OpenPage();
         }
-        
+
+        [When(@"I sign up with data")]
+        public void ISignUpWithData(Table table)
+        {
+            var signInModels = table.CreateInstance<SignInModel>();
+
+            _registrationPage.SetFirstName(signInModels.FirstName);
+            _registrationPage.SetSecondName(signInModels.LastName);
+            _registrationPage.SetEmail();
+            _registrationPage.SetPassword(signInModels.Password);
+            _registrationPage.SetConfirmPassword(signInModels.ConfirmPassword);
+            _registrationPage.SetPhomeNumber(signInModels.Password);
+
+            _registrationPage.ClickSubmit();
+        }
+
+        public class SignInModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string ConfirmPassword { get; set; }
+            public string Mobile { get; set; }
+
+            //public SignInModel()
+            //{
+            //    Email = $"asda2sd2asd{DateTime.Now:ddyyyymmHHmmssffff}@asdasd.ert";
+            //}
+        }
+
+        [Then(@"Next page is opened")]
+        public void NextPageIsOpened()
+        {
+            _registrationPage.OpenPage();
+        }
+
+
+
     }
 }
